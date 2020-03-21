0878ae9568e23f4b6ee435732ee7940292004446
UvanShankar <uvanmurugan@gmail.com>
2020-03-21 19:33:04 +0530

    user completed

--- app/src/main/java/com/example/instagram/UserTab.java
+++ app/src/main/java/com/example/instagram/UserTab.java
@@ -2,6 +2,7 @@ package com.example.instagram;
 
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,11 +10,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
@@ -25,9 +28,9 @@ import java.util.List;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class UserTab extends Fragment {
+public class UserTab extends Fragment implements AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener {
 ListView listView;
-ArrayList arrayList;
+ArrayList<String> arrayList;
 ArrayAdapter arrayAdapter;
     public UserTab() {
         // Required empty public constructor
@@ -42,6 +45,8 @@ ArrayAdapter arrayAdapter;
         listView=view.findViewById(R.id.listview);
         arrayList=new ArrayList();
         arrayAdapter=new ArrayAdapter(getContext(),android.R.layout.simple_list_item_1,arrayList);
+listView.setOnItemClickListener(UserTab.this);
+listView.setOnItemLongClickListener(UserTab.this);
         final TextView t=view.findViewById(R.id.textView3);
         ParseQuery<ParseUser> parseUserParseQuery=ParseUser.getQuery();
         parseUserParseQuery.whereNotEqualTo("username",ParseUser.getCurrentUser().getUsername());
@@ -56,10 +61,11 @@ ArrayAdapter arrayAdapter;
                         {
 
                             arrayList.add(p.getUsername());
-                           t.animate().alpha(0).setDuration(2000);
-                            listView.setAdapter(arrayAdapter);
-                            listView.setVisibility(View.VISIBLE);
+
                         }
+                        t.animate().alpha(0).setDuration(2000);
+                        listView.setAdapter(arrayAdapter);
+                        listView.setVisibility(View.VISIBLE);
                     }
                 }
 
@@ -69,4 +75,28 @@ ArrayAdapter arrayAdapter;
         return view;
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Intent intent=new Intent(getContext(),UsersPost.class);
+        intent.putExtra("username",arrayList.get(position));
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        ParseQuery<ParseUser> parseQuery=ParseUser.getQuery();
+        parseQuery.whereEqualTo("username",arrayList.get(position));
+        parseQuery.getFirstInBackground(new GetCallback<ParseUser>() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if(user!=null&&e==null)
+                {
+
+                }
+            }
+        });
+
+
+        return false;
+    }
 }
--- app/src/main/java/com/example/instagram/UsersPost.java
+++ app/src/main/java/com/example/instagram/UsersPost.java
@@ -2,13 +2,85 @@ package com.example.instagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class UsersPost extends AppCompatActivity {
-
+LinearLayout linearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_post);
+        linearLayout=(LinearLayout)findViewById(R.id.linearlay);
+        Intent recieved=getIntent();
+        final String username=recieved.getStringExtra("username");
+        this.setTitle(username+"'s Post");
+        ParseQuery<ParseObject> parseQuery=new ParseQuery("photo");
+        parseQuery.whereEqualTo("username",username);
+        parseQuery.orderByDescending("createdAt");
+        final ProgressDialog dialog=new ProgressDialog(this);
+        dialog.setMessage("Loading");
+        dialog.show();
+        parseQuery.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if(e==null&&objects.size()>0)
+                {
+                    for(ParseObject in :objects)
+                    {
+                        final TextView textView=new TextView(UsersPost.this);
+                        textView.setText(in.getString("imagedes"+""));
+                        ParseFile img=(ParseFile)in.get("picture");
+                        img.getDataInBackground(new GetDataCallback() {
+                            @Override
+                            public void done(byte[] data, ParseException e) {
+                                if(e==null&&data!=null)
+                                {
+                                    Bitmap bitmap= BitmapFactory.decodeByteArray(data,0,data.length);
+                                    ImageView imageView=new ImageView(UsersPost.this);
+                                    LinearLayout.LayoutParams imgparam=new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+                                    imgparam.setMargins(5,5,5,5);
+                                    imageView.setLayoutParams(imgparam);;
+                                    imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+
+                                    textView.setLayoutParams(imgparam);
+                                    textView.setGravity(Gravity.CENTER);
+                                    textView.setBackgroundColor(Color.BLUE);
+                                    textView.setTextColor(Color.WHITE);;
+                                    textView.setTextSize(30f);
+
+                                    linearLayout.addView(imageView);
+                                    linearLayout.addView(textView);
+                                }
+                            }
+                        });
+
+                    }
+                }else{
+                    Toast.makeText(UsersPost.this,username+" doesn't have any post ",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                dialog.dismiss();
+            }
+        });
     }
 }
